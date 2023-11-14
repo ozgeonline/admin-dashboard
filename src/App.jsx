44b0a4c7d943ae33d02c8559306cafd91b6aa629
ozgeonline <nisroc.414@hotmail.com>
2023-11-14 18:05:19 +0300

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import Geography from "./scenes/geography";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import Geography from "./scenes/geography";
 
 const App = () => {
 
@@ -38,9 +38,9 @@ const App = () => {
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/faq" element={<FAQ />} />
             <Route path="/bar" element={<Bar />} />
-            {/* <Route path="/pie" element={<Pie />} /> */}
-            {/* <Route path="/line" element={<Line />} /> */}
-            {/* <Route path="/geography" element={<Geography />} /> */}
+            <Route path="/pie" element={<Pie />} />
+            <Route path="/line" element={<Line />} />
+            <Route path="/geography" element={<Geography />} />
           </Routes>
         </main>
       </div>
